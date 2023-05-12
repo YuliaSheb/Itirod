7ea9fa27fb23b052231f32022bcea915e3d78269
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2023-05-12 20:29:35 +0300

    Add files via upload

---
+++ Itirod/javascript/process.js
@@ -0,0 +1,50 @@
+var indexVal = 0;
+function slideShow(){
+    setTimeout(slideShow,2000);
+    var x;
+    const img=document.getElementsByClassName("proc-img");
+    for(x=0;x<img.length;x++){
+        img[x].style.display="none";
+    }
+    indexVal++
+    if(indexVal>img.length){
+        indexVal=1;
+    }
+    img[indexVal-1].style.display = "block";
+}
+slideShow()
+
+document.addEventListener('DOMContentLoaded', function() {
+    // конечная дата, например 1 июля 2021
+    const deadline = new Date(2023, 05, 13);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+      $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+      $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+    // получаем элементы, содержащие компоненты даты
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
